6f6cc6397b6a1453e3d04ad4bf2a230ad731354d
Ivan Dorofeev <axxel123@rambler.ru>
2022-08-07 10:20:33 +0300

    Добавил .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # api_programmers_avg_salary
-Парвинг сайто HeadHunter и SuperJob, вывод анализа по запаботным платам
+
+Parsing HeadHunter и SuperJob and get average salary of the most populat programming languages.
+
+
+
+## Install
+
+First of all, install packages from requirements.txt:
+
+python pip -m install -r requirements.txt
+
+Then you need to create file ".env" and add variable:
+
+> SUPERJOB_SECRET_KEY = [....your secret_key...]
+
+
+Script take values of this variables and use:
+
+> headers = {'X-Api-App-Id': os.environ['SUPERJOB_SECRET_KEY']}
+
+```For more information or to get secret_key read:``` https://api.superjob.ru/
+
+## Run
+
+Open console in working directory and write:
+
+```python main.py```
+
+result:
+
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
-from statistics import mean
-
+from itertools import count
 import requests
 from dotenv import load_dotenv
+from terminaltables import AsciiTable
 
 
 def predict_rub_salary(vacancy):
@@ -16,31 +16,99 @@ def predict_rub_salary(vacancy):
     return None
 
 
-def main():
+def make_table(title, table):
+    TABLE_DATA = []
+
+    table_headers = ['Языки программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя заработная плата']
+    TABLE_DATA.append(table_headers)
+    for k, v in table.items():
+        TABLE_DATA.append([k, v['vacancies_found'], v['vacancies_processed'], v['average_salary']])
+
+    table_instance = AsciiTable(TABLE_DATA, title)
+    table_instance.justify_columns[4] = 'right'
+    print(table_instance.table)
+    print()
+
+
+def predict_rub_salary_for_superJob(vacantion):
+    if vacantion['payment_from'] not in [None, 0] or vacantion['payment_to'] not in [None, 0]:
+        avg_salary = int((vacantion['payment_from'] + vacantion['payment_to']) / 2)
+        return avg_salary
+    return None
+
+
+def superjob_avg_salary(languages):
     load_dotenv()
-    languages = ['python', 'java', 'goland', 'javascript']
-    lang_salaries = {}
+    languages_avg_salary = {}
     for language in languages:
-        url = 'https://api.hh.ru/vacancies'
-        vacancies_params = {'text': language,
-                            'area': 1,
-                            'period': 30,
-                            'per_page': 100
-                            }
-        vacancies = requests.get(url, params=vacancies_params)
-        vacancies_salary = []
-        vacancies_processed = len(vacancies.json()['items'])
-        for vacancy in vacancies.json()['items']:
-            avg_vacancy_salary = predict_rub_salary(vacancy)
-            if avg_vacancy_salary is not None:
-                vacancies_salary.append(avg_vacancy_salary)
-        if len(vacancies_salary) == 0:
-            average_salary = 0
+        language_data = {"vacancies_found": None,
+                         "vacancies_processed": None,
+                         "average_salary": None
+                         }
+        response = requests.get('https://api.superjob.ru/2.0/vacancies',
+                                headers={'X-Api-App-Id': os.environ['SUPERJOB_SECRET_KEY']},
+                                params={'keyword': language,
+                                        'published': 1,
+                                        'period': 7,
+                                        'town': 'Москва',
+                                        })
+        response.raise_for_status()
+
+        language_salaies = []
+        for i in response.json()['objects']:
+            avg_salary = predict_rub_salary_for_superJob(i)
+            if avg_salary is not None:
+                language_salaies.append(avg_salary)
+
+        if not len(language_salaies) == 0:
+            language_data['average_salary'] = int(sum(language_salaies) / len(language_salaies))
+            language_data['vacancies_processed'] = len(language_salaies)
+            language_data['vacancies_found'] = response.json()['total']
+
+        languages_avg_salary[language] = language_data
+
+    make_table('SuperJob', languages_avg_salary)
+
+
+def hh_avg_salary(languages):
+    languagies_avg_salary = {}
+    for language in languages:
+        salary = []
+        vacancies_on_page = []
+
+        for page in count():
+            url = 'https://api.hh.ru/vacancies'
+            vacancies_params = {'text': language,
+                                'area': 1,
+                                'period': 3,
+                                'page': page
+                                }
+            page_response = requests.get(url, params=vacancies_params)
+            page_response.raise_for_status()
+            page_data = page_response.json()
+            if page >= page_data['pages']:
+                break
+
+            vacancies_on_page.append(page_data['per_page'])
+
+            for vacancy in page_data['items']:
+                avg_salary = predict_rub_salary(vacancy)
+                if avg_salary is not None:
+                    salary.append(avg_salary)
+
+        if sum(vacancies_on_page) > page_data['found']:
+            vacancies_processed = page_data['found']
         else:
-            average_salary = int(sum(vacancies_salary) / len(vacancies_salary))
-        lang_salaries[language] = {'vacancies_processed': vacancies_processed, 'average_salary': average_salary}
-    print(lang_salaries)
+            vacancies_processed = sum(vacancies_on_page)
+
+        languagies_avg_salary[language] = {'average_salary': int(sum(salary) / len(salary)),
+                                           'vacancies_found': page_data['found'],
+                                           'vacancies_processed': vacancies_processed}
+
+    make_table('HeadHunter', languagies_avg_salary)
 
 
 if __name__ == '__main__':
-    main()
+    languages = ['python', 'java', 'goland', 'javascript', 'ruby', 'c++']
+    superjob_avg_salary(languages)
+    hh_avg_salary(languages)
